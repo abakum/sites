4d0768c823744480699b41284ca84d66d71b5497
abakum <koka.abakum@gmail.com>
2023-07-03 16:43:42 +0300

    wg async

--- main.go
+++ main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"context"
 	"os"
 	"path/filepath"
+	"sync"
 	"syscall"
 	"time"
 
 	"github.com/Trisia/gosysproxy"
 	"github.com/fsnotify/fsnotify"
 	"github.com/xlab/closer"
+	"golang.org/x/sys/windows"
 	"golang.org/x/sys/windows/registry"
+)
+
+const (
+	REG_NOTIFY_CHANGE_NAME     = uintptr(0x00000001)
+	REG_NOTIFY_CHANGE_LAST_SET = uintptr(0x00000004)
+	asyncTO                    = uint32(3000) // 3s
+	watchTO                    = time.Second
+)
+
+var (
+	wg sync.WaitGroup
 )
 
 func main() {
@@ -22,11 +36,14 @@ func main() {
 		advapi32 *syscall.DLL
 	)
 	defer closer.Close()
+	ctx, ca := context.WithCancel(context.Background())
 	closer.Bind(func() {
+		ca()
 		if err != nil {
 			let.Println(err)
 			defer os.Exit(1)
 		}
+		wg.Wait()
 		pressEnter()
 	})
 	// Create new watcher.
@@ -62,9 +79,16 @@ func main() {
 		err = srcError(err)
 		return
 	}
-	go anyWatch(regNotifyChangeKeyValue, registry.CURRENT_USER, `SOFTWARE\Policies\Microsoft\Internet Explorer\Control Panel`, "Autoconfig", 0, func() { PrintOk("gosysproxy.Off", gosysproxy.Off()) })
-	go anyWatch(regNotifyChangeKeyValue, registry.CURRENT_USER, `SOFTWARE\Policies\Microsoft\Windows\Control Panel\Desktop`, "ScreenSaveActive", "0", nil)
-	go anyWatch(regNotifyChangeKeyValue, registry.CURRENT_USER, `SOFTWARE\Policies\YandexBrowser`, "ProxyMode", "direct", nil)
+	go anyWatch(regNotifyChangeKeyValue, ctx, registry.CURRENT_USER, `SOFTWARE\Policies\Microsoft\Internet Explorer\Control Panel`, "Autoconfig", 0, func() { PrintOk("gosysproxy.Off", gosysproxy.Off()) })
+	// go anyWatch(regNotifyChangeKeyValue, registry.CURRENT_USER, `Software\Microsoft\Windows\CurrentVersion\Internet Settings`, "ProxyEnable", 0, func() { PrintOk("gosysproxy.Off", gosysproxy.Off()) })
+	go anyWatch(regNotifyChangeKeyValue, ctx, registry.CURRENT_USER, `SOFTWARE\Policies\YandexBrowser`, "ProxyMode", "direct", nil)
+	go anyWatch(regNotifyChangeKeyValue, ctx, registry.CURRENT_USER, `SOFTWARE\Policies\Microsoft\Windows\Control Panel\Desktop`, "ScreenSaveActive", "0", nil)
+	go anyWatch(regNotifyChangeKeyValue, ctx, registry.CURRENT_USER, `SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System`, "DisableLockWorkstation", 1, nil)
+
+	// go func() {
+	// 	time.Sleep(time.Second * 3) // test cancel async
+	// 	ca() // test cancel anyWatch
+	// }()
 
 	// Start listening for events.
 	for {
@@ -85,92 +109,135 @@ func main() {
 	}
 }
 
-func anyWatch(regNotifyChangeKeyValue *syscall.Proc, root registry.Key, path, key string, val any, after func()) {
-	fn := func(k registry.Key, key string, val any) error {
-		return Errorf("wrong type of val")
+func anyWatch(regNotifyChangeKeyValue *syscall.Proc, ctx context.Context, root registry.Key, path, key string, val any, after func()) {
+	wg.Add(1)
+	defer wg.Done()
+	fn := func(k registry.Key) (bool, error) {
+		return false, Errorf("wrong type of val")
 	}
 	switch value := val.(type) {
 	case int:
-		fn = func(k registry.Key, key string, val any) error {
+		fn = func(k registry.Key) (bool, error) {
 			old, _, err := k.GetIntegerValue(key)
 			if uint32(old) != uint32(value) || err != nil {
 				err = k.SetDWordValue(key, uint32(value))
 				if err != nil {
-					return srcError(err)
+					return false, srcError(err)
 				}
-				ltf.Printf(`%s\%s %v->%v`, path, key, old, val)
-				if after != nil {
-					after()
-				}
+				ltf.Printf(`%s\%s %v->%v`, path, key, old, value)
+				return true, nil
 			}
-			return nil
+			return false, nil
 		}
 	case uint32:
-		fn = func(k registry.Key, key string, val any) error {
+		fn = func(k registry.Key) (bool, error) {
 			old, _, err := k.GetIntegerValue(key)
 			if uint32(old) != value || err != nil {
 				err = k.SetDWordValue(key, value)
 				if err != nil {
-					return srcError(err)
+					return false, srcError(err)
 				}
-				ltf.Printf(`%s\%s %v->%v`, path, key, old, val)
-				if after != nil {
-					after()
-				}
+				ltf.Printf(`%s\%s %v->%v`, path, key, old, value)
+				return true, nil
 			}
-			return nil
+			return false, nil
 		}
 	case uint64:
-		fn = func(k registry.Key, key string, val any) error {
+		fn = func(k registry.Key) (bool, error) {
 			old, _, err := k.GetIntegerValue(key)
 			if uint64(old) != value || err != nil {
 				err = k.SetQWordValue(key, value)
 				if err != nil {
-					return srcError(err)
+					return false, srcError(err)
 				}
-				ltf.Printf(`%s\%s %v->%v`, path, key, old, val)
-				if after != nil {
-					after()
-				}
+				ltf.Printf(`%s\%s %v->%v`, path, key, old, value)
+				return true, nil
 			}
-			return nil
+			return false, nil
 		}
 	case string:
-		fn = func(k registry.Key, key string, val any) error {
+		fn = func(k registry.Key) (bool, error) {
 			old, _, err := k.GetStringValue(key)
 			if old != value || err != nil {
 				err = k.SetStringValue(key, value)
 				if err != nil {
-					return srcError(err)
+					return false, srcError(err)
 				}
-				ltf.Printf(`%s\%s %v->%v`, path, key, old, val)
-				if after != nil {
-					after()
-				}
+				ltf.Printf(`%s\%s %v->%v`, path, key, old, value)
+				return true, nil
 			}
-			return nil
+			return false, nil
 		}
 	}
 	for {
-		time.Sleep(time.Second)
-		k, err := registry.OpenKey(root, path, registry.QUERY_VALUE|registry.SET_VALUE)
-		if err != nil {
-			letf.Println(err)
-			continue
-		} else {
-			err = fn(k, key, val)
-			k.Close()
+		sec := time.NewTimer(watchTO)
+		select {
+		case <-ctx.Done():
+			ltf.Println(key, "ctx.Done")
+			return
+		case <-sec.C:
+			k, err := registry.OpenKey(root, path, registry.QUERY_VALUE|registry.SET_VALUE|syscall.KEY_NOTIFY)
 			if err != nil {
 				letf.Println(err)
 				continue
 			}
-		}
-		k, err = registry.OpenKey(root, path, syscall.KEY_NOTIFY)
-		if err != nil {
-			letf.Println(err)
-		} else {
-			regNotifyChangeKeyValue.Call(uintptr(k), 0, 0x00000001|0x00000004, 0, 0)
+			ok, err := fn(k)
+			if err != nil {
+				let.Println(err)
+				k.Close()
+				continue
+			}
+			if ok && after != nil {
+				after()
+			}
+			// bloking wait
+			// regNotifyChangeKeyValue(key windows.Handle, watchSubtree bool, notifyFilter uint32, event windows.Handle, asynchronous bool) (regerrno error)
+			// regNotifyChangeKeyValue.Call(uintptr(k), 0, REG_NOTIFY_CHANGE_NAME|REG_NOTIFY_CHANGE_LAST_SET, 0, 0)
+			done, err := async(regNotifyChangeKeyValue, ctx, k)
 			k.Close()
+			if err != nil {
+				let.Println(err)
+				continue
+			}
+			if done {
+				ltf.Println(key, "async ctx.Done")
+				return
+			}
+
+		}
+	}
+}
+
+// https://git.zx2c4.com/wireguard-go/tree/tun/wintun/registry/registry_windows.go?id=5ca1218a5c16fb9b5e99b61c0b5758f66087e2e4
+func async(regNotifyChangeKeyValue *syscall.Proc, ctx context.Context, k registry.Key) (bool, error) {
+	event, err := windows.CreateEvent(nil, 0, 0, nil)
+	if err != nil {
+		letf.Println(err)
+		return false, err
+	}
+	defer windows.CloseHandle(event)
+
+	for {
+		select {
+		case <-ctx.Done():
+			return true, nil
+		default:
+			r0, _, err := regNotifyChangeKeyValue.Call(uintptr(k), 0, REG_NOTIFY_CHANGE_NAME|REG_NOTIFY_CHANGE_LAST_SET, uintptr(windows.Handle(event)), 1)
+			// PrintOk(fmt.Sprintf("regNotifyChangeKeyValue %v", r0), err)
+			if r0 != 0 {
+				letf.Println(err)
+				return false, err
+			}
+			// bloking wait with timeout
+			s, err := windows.WaitForSingleObject(event, asyncTO)
+			if err != nil {
+				letf.Println(err)
+				return false, err
+			}
+			if s == uint32(windows.WAIT_TIMEOUT) {
+				continue
+			}
+			return false, nil
 		}
 	}
 }
